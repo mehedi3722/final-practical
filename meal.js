a3af1da187e05941d64d13eaa12223a230df452a
unknown <2030167@iub.edu.bd>
2024-04-26 10:15:32 +0600

    first commit

---
+++ meal.js
@@ -0,0 +1,6 @@
+const loadMeals = (searchText) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showMeals(data.meals));
+}
